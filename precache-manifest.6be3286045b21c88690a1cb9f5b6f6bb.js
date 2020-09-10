98662ace2418b79f6c2bc9b9e14fce68839a523a
Omid Heidari <omid_heidari90@yahoo.com>
2020-09-10 15:39:26 +0430

    Updates

---
+++ precache-manifest.6be3286045b21c88690a1cb9f5b6f6bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd99997ac1174cdefa55727c27ef2c44",
+    "url": "/Whatsapp/index.html"
+  },
+  {
+    "revision": "60d5cd9090fb112eca4a",
+    "url": "/Whatsapp/static/css/main.874f8f63.chunk.css"
+  },
+  {
+    "revision": "0e571f499765ca127585",
+    "url": "/Whatsapp/static/js/2.ead05c00.chunk.js"
+  },
+  {
+    "revision": "0b193cffd0567a6da743599aadaf1b7b",
+    "url": "/Whatsapp/static/js/2.ead05c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d5cd9090fb112eca4a",
+    "url": "/Whatsapp/static/js/main.8e2a4780.chunk.js"
+  },
+  {
+    "revision": "797bd66ad25b0ba93e80",
+    "url": "/Whatsapp/static/js/runtime-main.659b56d8.js"
+  },
+  {
+    "revision": "0e878a0fa68c61b06e781cee2e6bc71f",
+    "url": "/Whatsapp/static/media/WhatsApp.0e878a0f.svg"
+  }
+]);
